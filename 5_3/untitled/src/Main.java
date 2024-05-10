de0de14b952926b52a82a94f6162b982f5c3cf34
gugora06 <161825048+gugora06@users.noreply.github.com>
2024-05-10 10:59:47 +0300

    Add files via upload

---
+++ 5_3/untitled/src/Main.java
@@ -0,0 +1,61 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+////1 завдання
+//        int a = 125;
+//        if (a > 50 && a < 100) {
+//            System.out.println("Число " + a + " входить до проміжку (50;100)");
+//        }else{
+//            System.out.println("Число " + a + " не входить до проміжку (50;100)");
+//        }
+//3 завдання
+//        int myFloor = 1;
+//        int wantFloor = 2;
+//        if(myFloor < wantFloor) {
+//            System.out.println("Ви піднялись на " + wantFloor + " поверх");
+//        } else if (myFloor > wantFloor && wantFloor == 2){
+//            System.out.println("Ви опустились на 1 поверх");
+//        } else if (myFloor < wantFloor && wantFloor == 2) {
+//            System.out.println("Ви піднялись на 3 поверх");
+//        }else{
+//            System.out.println("Ви опустились на " + wantFloor + " поверх");
+//        }
+////2 завдання
+//        int a = 545;
+//        int b = a / 100; //1
+//        int d = a % 100;
+//        int e = d / 10; //2
+//        int c = a % 10; //3
+//        if(b >= c & b >= e){
+//            System.out.print(b);
+//        } else if (c >= b & c >= e) {
+//            System.out.println(c);
+//        }else{
+//            System.out.println(e);
+//        }
+//4 завдання
+//        String opt = "asdasdasd";
+//
+//        switch (opt){
+//            case ("Так"):
+//            case ("OK"):
+//            case ("Yes"):
+//            case ("Y"):
+//            case ("+"):
+//            case ("Ok"):
+//                System.out.println("Я погоджуюсь!");
+//                break;
+//            case ("Ні"):
+//            case ("NO"):
+//            case ("N"):
+//            case ("-"):
+//            case ("No"):
+//                System.out.println("Я відмовляюсь!");
+//                break;
+//            default:
+//
+        }
+
+    }
+}
